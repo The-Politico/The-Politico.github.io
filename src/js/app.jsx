12e271c81d59fbf19cb2e1003de51a7e11d44186
Jon McClure <jmcclure@politico.com>
2018-01-28 23:25:06 -0500

    build:

---
+++ src/js/app.jsx
@@ -0,0 +1,10 @@
+import './../../node_modules/normalize.css/normalize.css';
+import './../scss/styles.scss';
+
+import { h } from 'preact';
+import React from 'react';
+import { render } from 'react-dom';
+// 
+// render((
+//   <div><h1>Hello</h1></div>
+// ), document.getElementById('app'));
