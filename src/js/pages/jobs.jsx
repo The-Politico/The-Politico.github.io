61a441b9e7a998e8c6c6676dc21e086da00a807e
Jon McClure <jmcclure@politico.com>
2018-03-26 12:21:38 -0400

    apply buttons

--- src/js/pages/jobs.jsx
+++ src/js/pages/jobs.jsx
@@ -19,6 +19,12 @@ const Jobs = () => (
           className="button"
           href="mailto:jmcclure@politico.com,lmihalik@politico.com?subject=POLITICO%20Interactive%20News%20Jobs%20Developer"
         >Get in touch</a>
+        <a
+          className="button"
+          href="https://recruiting.ultipro.com/PER1013PCLL/JobBoard/b972ff6a-41b7-4e97-9c71-273c2595c77d/OpportunityDetail?opportunityId=7b72921e-cdce-4cd4-8f71-acc2c9d35bbc"
+        >
+          Apply
+        </a>
       </div>
       <div id="designer" className='card'>
         <Designer />
@@ -28,6 +34,12 @@ const Jobs = () => (
           className="button"
           href="mailto:jmcclure@politico.com,lmihalik@politico.com?subject=POLITICO%20Interactive%20News%20Jobs%20Designer"
         >Get in touch</a>
+        <a
+          className="button"
+          href="https://recruiting.ultipro.com/PER1013PCLL/JobBoard/b972ff6a-41b7-4e97-9c71-273c2595c77d/OpportunityDetail?opportunityId=d2105718-f882-422c-80e4-bc7bc7117610"
+        >
+          Apply
+        </a>
       </div>
     </article>
   </div>
