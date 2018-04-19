3175f7bff2b1280de4fbb7944ae41165ad665ac4
Jon McClure <jmcclure@politico.com>
2018-04-19 11:49:07 -0400

    update

--- src/js/pages/jobs.jsx
+++ src/js/pages/jobs.jsx
@@ -3,7 +3,6 @@ import Header from '../components/header.jsx';
 
 import About from '../../content/jobs/about.md';
 import Designer from '../../content/jobs/designer.md';
-import Developer from '../../content/jobs/developer.md';
 
 const Jobs = () => (
   <div className='page jobs'>
@@ -11,21 +10,6 @@ const Jobs = () => (
     <article>
       <h2>Jobs</h2>
       <About />
-      <div id="designer" className='card'>
-        <Developer />
-      </div>
-      <div className='card'>
-        <a
-          className="button"
-          href="mailto:jmcclure@politico.com,lmihalik@politico.com?subject=POLITICO%20Interactive%20News%20Jobs%20Developer"
-        >Get in touch</a>
-        <a
-          className="button"
-          href="https://recruiting.ultipro.com/PER1013PCLL/JobBoard/b972ff6a-41b7-4e97-9c71-273c2595c77d/OpportunityDetail?opportunityId=7b72921e-cdce-4cd4-8f71-acc2c9d35bbc"
-        >
-          Apply
-        </a>
-      </div>
       <div id="designer" className='card'>
         <Designer />
       </div>
