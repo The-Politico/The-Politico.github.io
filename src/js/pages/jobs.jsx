f9a9a6fcce6532b8d764beb45d886411e57926f7
Jon McClure <jmcclure@politico.com>
2018-07-10 11:14:48 -0400

    application

--- src/js/pages/jobs.jsx
+++ src/js/pages/jobs.jsx
@@ -18,6 +18,12 @@ const Jobs = () => (
           className="button"
           href="mailto:jmcclure@politico.com,lmihalik@politico.com?subject=POLITICO%20Interactive%20News%20Jobs%20Graphics%20Reporter"
         >Get in touch</a>
+        <a
+          className="button"
+          href="https://recruiting.ultipro.com/PER1013PCLL/JobBoard/b972ff6a-41b7-4e97-9c71-273c2595c77d/OpportunityDetail?opportunityId=0d3cfe8f-39a0-4b92-b6b5-42825bbd6486"
+        >
+          Apply
+        </a>
       </div>
     </article>
   </div>
